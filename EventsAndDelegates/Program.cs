899a9652261808d4d3d741e9c7ecd08a2b30dd37
atma0004 <atma0004@edu.eal.dk>
2017-05-27 16:30:24 +0200

    Add project files.

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video()
+            {
+                Title = "Video 1"
+            };
+            var videoEncoder = new VideoEncoder();
+            var mailService = new MailService();
+            var messageService = new MessageServece();
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+            videoEncoder.Encode(video);
+        }
+    }
+    public class MailService
+    {
+        public void OnVideoEncoded(object source , EventArgs e)
+        {
+            Console.WriteLine("MaliService: Sending an email......");
+        }
+    }
+    public class MessageServece
+    {
+        public void OnVideoEncoded(object source , EventArgs args)
+        {
+            Console.WriteLine("MessageService: Sending a text message ....");
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class VideoEncoder
+    {
+        public delegate void VideoEncodedEventHandler(object source, EventArgs arge);
+        public event VideoEncodedEventHandler VideoEncoded;
+        public void Encode(Video Vidio)
+        {
+            
+            Console.WriteLine("Encoding Video......");
+            Thread.Sleep(3000);
+            OnVideoEncoded();
+        }
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
+        }
+    }
+}
